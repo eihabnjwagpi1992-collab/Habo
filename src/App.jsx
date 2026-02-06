477da3dc73415cdd6d28d09f1551947ef966e1ee
eihabnjwagpi1992-collab <eihabnjwagpi1992@gmail.com>
2026-02-06 18:45:09 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,6 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
-import NotFound from "./pages/NotFound";
 
 function App() {
   return (
