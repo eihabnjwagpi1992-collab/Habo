1bc983b7b564aceb3f556210f54573a4217667f1
eihabnjwagpi1992-collab <eihabnjwagpi1992@gmail.com>
2026-02-06 10:10:43 +0300

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+// App.jsx
+
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Dashboard from "./pages/Dashboard";
+import NotFound from "./pages/NotFound";
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
